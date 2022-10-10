9f1c05c56f6da2de8f2e52d1f9346c5b125c77be
Claire-art <juachef@gmail.com>
2022-10-10 15:10:50 +0900

    todolist 1차 구현

--- backend/project/club/urls.py
+++ backend/project/club/urls.py
@@ -5,4 +5,8 @@ from . import views
 app_name = 'club'
 urlpatterns = [
     path('', views.club_list_create),
+    path('todolist',todolist,name='list'),
+    path('create/',todocreate, name='create'),
+    path('update/<str:pk>/',todoupdate,name="update"),
+    path('delete/<str:pk>/',tododelete, name='delete')
 ]
